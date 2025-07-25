5a5299b01da0f2366072958182189b2096a7b60a
agustinbutrico <agustinbutrico@gmail.com>
2025-07-25 06:04:11 -0300

    Add project files.

---
+++ TexturesLib.Shared/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+
+namespace TexturesLib.Shared
+{
+    [BepInPlugin("AgusBut.TexturesLib.Shared", "TexturesLib.Shared", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Plugin Instance { get; private set; }
+        public static BepInEx.Logging.ManualLogSource Log { get; private set; }
+
+        private void Awake()
+        {
+            Instance = this;
+            Log = base.Logger;
+
+            Logger.LogInfo("TexturesLib.Shared loaded successfully.");
+        }
+    }
+}
---
+++ TexturesLib.Shared/SpriteHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace TexturesLib.Shared
+{
+    public static class SpriteHelper
+    {
+        private static readonly Dictionary<string, Sprite> _spriteCache = new Dictionary<string, Sprite>();
+
+        public static Sprite FindSpriteByName(string spriteName)
+        {
+            if (_spriteCache.TryGetValue(spriteName, out var sprite))
+                return sprite;
+
+            foreach (var s in Resources.FindObjectsOfTypeAll<Sprite>())
+            {
+                if (s.name == spriteName || s.name == spriteName + "_TexturesLib")
+                {
+                    _spriteCache[spriteName] = s;
+                    return s;
+                }
+            }
+
+            Plugin.Log.LogWarning($"Sprite '{spriteName}' not found in memory.");
+            return null;
+        }
+    }
+}
